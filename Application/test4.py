4909ccbb38a376625d0ad290c08d09161cba0cad
isabelle-nobile <isabelle.nobile@laplateforme.io>
2023-03-16 20:33:33 +0100

    v8 woking

--- Application/test4.py
+++ Application/test4.py
@@ -13,30 +13,45 @@ class Alarme:
         print("L'heure de l'alarme est atteinte!")
         self.active = False
 
+    def regler_heure(self, heure):
+        self.heure = heure
+        self.active = True
+
+
 class Horloge:
     def __init__(self, heure, alarme):
         self.heure = heure
         self.alarme = alarme
 
-    def afficher_heure(self):
-        while True:
-            heure_actuelle = time.localtime()
-            heures, minutes, secondes = heure_actuelle.tm_hour, heure_actuelle.tm_min, heure_actuelle.tm_sec
-            print(f"{self.heure[0]:02d}:{self.heure[1]:02d}:{self.heure[2]:02d}", end="\r")
+    def afficher_compte_a_rebours(self):
+        heures, minutes, secondes = self.heure
+
+        while heures >= 0:
+            print(f"{heures:02d}:{minutes:02d}:{secondes:02d}", end="\r")
             time.sleep(1)
-            if (heures, minutes, secondes) == self.heure:
-                self.alarme.heure_atteinte()
-                break
-            elif self.alarme.active and (heures, minutes, secondes) == self.alarme.heure:
-                self.alarme.declencher()
-                break
-            elif not self.alarme.active:
-                print("L'alarme a été arrêtée.")
+
+            if heures == 0 and minutes == 0 and secondes == 0:
+                print("Le temps est écoulé!")
                 break
 
+            secondes -= 1
+            if secondes < 0:
+                minutes -= 1
+                secondes = 59
+            if minutes < 0:
+                heures -= 1
+                minutes = 59
 
+        if self.alarme.active:
+            print("L'alarme n'a pas été déclenchée.")
+        else:
+            print("L'alarme a été arrêtée.")
+
+        self.alarme.active = True
+
+heure = (16, 30, 20)
 alarme = Alarme()
-alarme.heure = (15, 15, 0)
-horloge = Horloge((15, 15, 15), alarme)
-print("L'heure de l'alarme est réglée sur :", alarme.heure[0], ":", alarme.heure[1], ":", alarme.heure[2])
-horloge.afficher_heure()
+alarme.regler_heure((16, 30, 0))
+horloge = Horloge(heure, alarme)
+horloge.afficher_compte_a_rebours()
+
